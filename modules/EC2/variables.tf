d45a48ac3d31275a82aa943c2569c2781e386624
= <mohamed202116370@gmail.com>
2024-03-19 04:28:32 +0200

    f

---
+++ modules/EC2/variables.tf
@@ -0,0 +1,10 @@
+variable "CPU" {
+  default = "t2.micro"
+}
+variable "AMI" {
+    default = "ami-07d9b9ddc6cd8dd30"
+}
+
+variable JUMP_SG_ID{}
+variable "PUB_SUB_1_A_ID"{}
+variable "KEY_NAME"{}
---
+++ modules/cloudfront/variables.tf
@@ -0,0 +1,8 @@
+variable "CERTIFICATE_DOMAIN_NAME"{
+     type        = string
+}
+variable "ALB_DOMAIN_NAME" {}
+variable "ADDITIONAL_DOMAIN_NAME" {
+     type        = string
+}
+variable "PROJECT_NAME" {}
