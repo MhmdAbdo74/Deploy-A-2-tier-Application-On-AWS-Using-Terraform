0e6d5a37b8562287d1a0bd20541698ac02f57d4a
= <mohamed202116370@gmail.com>
2024-03-19 04:18:15 +0200

    f

--- modules/cloudfront/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "CERTIFICATE_DOMAIN_NAME"{
-     type        = string
-}
-variable "ALB_DOMAIN_NAME" {}
-variable "ADDITIONAL_DOMAIN_NAME" {
-     type        = string
-}
-variable "PROJECT_NAME" {}
--- modules/route53/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "HOSTED_ZONE_NAME"{
-    default = "devopsisculture.space"
-}
-
-variable "CLOUDFRONT_HOSTED_ZONE_ID" {}
-
-variable "CLOUDFRONT_DOMAIN_NAME" {}
